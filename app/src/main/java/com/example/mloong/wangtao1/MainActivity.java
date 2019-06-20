4b350c808bd10e9a1e920926dfcd8f31fa83d121
wangtaoss <1433953566@qq.com>
2019-06-20 15:34:11 +0800

    3

--- app/src/main/java/com/example/mloong/wangtao1/MainActivity.java
+++ app/src/main/java/com/example/mloong/wangtao1/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //test6
         setContentView(R.layout.activity_main);
     }
 }
